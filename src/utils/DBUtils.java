46be7b83239adabc840e8c46ce2e59a0f63b4b05
Milos <milos.vujasinovic.98@gmail.com>
2019-04-24 23:33:38 +0200

    Better code organisation

--- src/utils/DBUtils.java
+++ src/utils/DBUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Types;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -10,6 +11,23 @@ import java.util.Map;
 
 public final class DBUtils {
 	private DBUtils() {}
+	
+	private static Object getColumnValue(ResultSet rset, ResultSetMetaData metaData, int column) 
+			throws SQLException {
+		Object result = null;
+		switch(metaData.getColumnType(column)) {
+			case Types.INTEGER:
+				result = rset.getInt(column);
+				break;
+			case Types.VARCHAR:
+				result = rset.getString(column);
+				break;
+			case Types.DATE:
+				result = rset.getDate(column);
+				break;
+		}
+		return result;
+	}
 	
 	public static List<Map<String, Object>> resultSetToListOfMaps(ResultSet rset) {
 		if(rset == null) {
@@ -26,18 +44,8 @@ public final class DBUtils {
 				for(int i = 0; i < columnCount; i++) {
 					int column = i + 1;
 					String imeKolone = metaData.getColumnName(column);
-					Object vrednost = null;
-					switch(metaData.getColumnType(column)) {
-						case Types.INTEGER:
-							vrednost = rset.getInt(column);
-							break;
-						case Types.VARCHAR:
-							vrednost = rset.getString(column);
-							break;
-						case Types.DATE:
-							vrednost = rset.getDate(column);
-							break;
-					}
+					Object vrednost = getColumnValue(rset, metaData, column);
+					
 					if(vrednost != null) {
 						mapa.put(imeKolone, vrednost);
 					}
