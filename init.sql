530c090e28cb24ce5490e3911d510b46be4e73b8
Milos <milos.vujasinovic.98@gmail.com>
2019-04-24 22:49:35 +0200

    Changes made to database initialization

--- init.sql
+++ init.sql
@@ -1,6 +1,21 @@
-CREATE TABLE Korisnik (
-	id INTEGER PRIMARY KEY,
-	ime TEXT NOT NULL,
-	prezime TEXT NOT NULL
+CREATE TABLE Uloga (
+	u_id 	INTEGER PRIMARY KEY,
+	naziv 	TEXT NOT NULL
 );
-INSERT INTO Korisnik(ime, prezime) VALUES ('Pera', 'Peric')
+
+CREATE TABLE Korisnik (
+	k_id		INTEGER PRIMARY KEY,
+	ime			TEXT NOT NULL,
+	prezime		TEXT NOT NULL,
+	username	TEXT UNIQUE NOT NULL,
+	password	TEXT NOT NULL,
+	uloga_id	INTEGER NOT NULL,
+	FOREIGN KEY(uloga_id) REFERENCES Uloga(u_id)
+);
+
+INSERT INTO Uloga(u_id, naziv) VALUES(0, 'profesor');
+INSERT INTO Uloga(u_id, naziv) VALUES(1, 'student');
+INSERT INTO Korisnik(ime, prezime, username, password, uloga_id) 
+	VALUES('Mika', 'Mikic', 'mikamika', 'sifra123', 0);
+INSERT INTO Korisnik(ime, prezime, username, password, uloga_id) 
+	VALUES('Pera', 'Peric', 'perica', 'sifra123', 1);
--- setupDB.py
+++ setupDB.py
@@ -1,11 +1,20 @@
 import sqlite3
 import os 
 
+# Ovaj fajl inicijalizuje bazu podataka
+# Ako baza vec postoji obrisace je i napravice novu
+# Java kod nece raditi bez baze
+# Za sve sitnice kao sto su obavestenje da nema baze ili pitanje korisnika da li hoce da resetuje bazu kriva je moja lenjost sto ne postoje
+
 DB_NAME = 'db.db'
 INIT_NAME = 'init.sql'
 DIR = os.path.dirname(os.path.realpath(__file__))
 SQL_INIT_FILE_PATH = os.path.join(DIR, INIT_NAME)
 DB_PATH = os.path.join(DIR, DB_NAME)
+
+if os.path.exists(DB_PATH):
+	print('Stara baza se brise i pravi se nova')
+	os.remove(DB_PATH)
 
 conn = sqlite3.connect(DB_PATH)
 
