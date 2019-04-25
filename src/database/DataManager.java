ce00578a6b4a644eafd5d756bb795799686621c7
Milos <milos.vujasinovic.98@gmail.com>
2019-04-25 09:25:42 +0200

    User retrieving methods and close added

--- src/database/DataManager.java
+++ src/database/DataManager.java
@@ -1,5 +1,12 @@
 package database;
 
+import java.util.List;
+
+import models.Korisnik;
+
 public interface DataManager {
+	default void close() {}
 	
+	List<Korisnik> getAllProfesor();
+	List<Korisnik> getAllStudent();
 }
