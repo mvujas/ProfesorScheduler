f19882d8721c40add09be0726c67b8805e5f2186
Milos <milos.vujasinovic.98@gmail.com>
2019-04-25 09:26:54 +0200

    Testing class updated

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,17 @@
 import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 import database.ConnectionPool;
+import database.DataManager;
+import database.SQLDataManager;
+import database.transactions.KorisnikTransactions.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			ConnectionPool ds = new ConnectionPool("jdbc:sqlite:db.db");
-			
-			for(int i = 0; i < 100; i++) {
-				Connection conn = ds.getConnection();
-				try(Statement stmt = conn.createStatement();
-						ResultSet rset = stmt.executeQuery("SELECT * FROM Korisnik")) {
-					while(rset.next()) {
-						System.out.printf(
-								"%d %s %s\n",
-								rset.getInt("id"),
-								rset.getString("ime"),
-								rset.getString("prezime"));
-					}
-				}
-				ds.releaseConnection(conn);
-			}
+			DataManager db = new SQLDataManager("db.db");
+			System.out.println(db.getAllProfesor());
+			db.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
